e2be527cdbe8c3498f4b924cbefd996c0781e27c
Amgad226 <amgad.wr.1@gmail.com>
2023-11-18 23:48:12 +0200

    [Feature] (app.module) implement access auth guard on all routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,8 @@ import { GoogleDriveModule } from './google-drive/google-drive.module';
 import { GoogleDriveService } from './google-drive/google-drive.service';
 import { MyConfigService } from './my-config/my-config.service';
 import { UsersModule } from './users/users.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth/guards/access-auth.guard';
 
 @Module({
   imports: [
@@ -26,6 +28,14 @@ import { UsersModule } from './users/users.module';
     UsersModule,
   ],
   controllers: [AppController, DiskSpaceController],
-  providers: [AppService, GoogleDriveService, MyConfigService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    AppService,
+    GoogleDriveService,
+    MyConfigService,
+  ],
 })
 export class AppModule {}
