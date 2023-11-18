0915104a2a63d5a32d89be200b07af01c4d8bd86
Amgad226 <amgad.wr.1@gmail.com>
2023-11-18 22:57:01 +0200

    [Fix] (src/app-controller)remove tested endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,11 @@
-import {
-  Controller,
-  FileTypeValidator,
-  Get,
-  MaxFileSizeValidator,
-  ParseFilePipe,
-  Post,
-  UploadedFile,
-  UseInterceptors,
-} from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
-import { GoogleDriveService } from './google-drive/google-drive.service';
-import { ReadableStreamBuffer } from 'stream-buffers';
-import { Readable } from 'stream';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly googleDriveService: GoogleDriveService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
@@ -40,18 +25,5 @@ export class AppController {
   )
   async uploadFile() {
     return 'file store successfully';
-  }
-
-  @Post('/upload-drive')
-  @UseInterceptors(FileInterceptor('file'))
-  async uploadToDrive(@UploadedFile() file: Express.Multer.File) {
-    try {
-  
-    const nest_source_safe_folder = '1T_0BsIBtv4nywGDHAB2yQocw9RRhceUw';
-
-      return await this.googleDriveService.uploadFile(file,nest_source_safe_folder);
-    } catch (error) {
-      return `Failed to upload file: ${error.message}`;
-    }
   }
 }
