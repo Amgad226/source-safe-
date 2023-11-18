24fb239876921dd294c34a6d1360a780c064fab2
Amgad226 <amgad.wr.1@gmail.com>
2023-11-18 22:54:55 +0200

    [Feature] (src/app-module) add import required classes in other modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,31 @@
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { DiskSpaceController } from './disk-space.controller';
+import { GoogleDriveModule } from './google-drive/google-drive.module';
 import { GoogleDriveService } from './google-drive/google-drive.service';
+import { MyConfigService } from './my-config/my-config.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    BullModule.forRootAsync({
+      useFactory: () => ({
+        redis: {
+          host: 'localhost',
+          port: 6379,
+        },
+      }),
+    }),
+    GoogleDriveModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController, DiskSpaceController],
-  providers: [AppService, GoogleDriveService],
+  providers: [AppService, GoogleDriveService, MyConfigService],
 })
 export class AppModule {}
