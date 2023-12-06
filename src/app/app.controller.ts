201ac9ca7844bcbc1ba19ec93824ea26896d3b0a
Amgad226 <amgad.wr.1@gmail.com>
2023-12-06 21:01:56 +0300

    create host name api

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,13 +1,31 @@
-import { Controller, Get, Post, Req, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Headers,
+  Post,
+  Req,
+  UseInterceptors,
+} from '@nestjs/common';
+
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { AppService } from './app.service';
 import { Public } from '../decorators/public.decorators';
 import { TokenPayload } from 'src/decorators/user-decorator';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Public()
+  @Get('host-name')
+  hostName(@Headers() headers,@Req() req: Request) {
+    const upstreamServerInfo = req.headers['x-server-info'];
+    return `Hello! Request received from ${upstreamServerInfo}.`;
+
+    return headers.host +'  ' + req.hostname; ;
+  }
 
   @Get()
   getUserByToken(@TokenPayload() tokenPayload) {
