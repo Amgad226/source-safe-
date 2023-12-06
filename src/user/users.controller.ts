d7ba98f6bc921b2bf005eace00db2fe1c694c7f5
Amgad226 <amgad.wr.1@gmail.com>
2023-12-07 01:58:32 +0300

    create accept ,reject and index user join folder

--- src/user/users.controller.ts
+++ src/user/users.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { UserEntity } from 'src/auth/entities/common/user-entity';
 import { BaseModuleController } from 'src/base-module/base-module.controller';
 import { ResponseInterface } from 'src/base-module/response.interface';
 import { Public } from 'src/decorators/public.decorators';
 import { UsersService } from './users.service';
+import { TokenPayloadProps } from 'src/base-module/token-payload-interface';
+import { TokenPayload } from 'src/decorators/user-decorator';
 
 @Controller('user')
 export class UsersController extends BaseModuleController {
@@ -17,4 +19,39 @@ export class UsersController extends BaseModuleController {
     const user = await this.userService.user();
     return this.successResponse({ data: user });
   }
+
+  @Get('folder-requests')
+  async folderRequest(
+    @TokenPayload() tokenPayload: TokenPayloadProps,
+  ): Promise<ResponseInterface> {
+    const folderRequest = await this.userService.folderRequest(tokenPayload);
+    return this.successResponse({ data: folderRequest });
+  }
+
+  @Post('accept-join-folder/:folder_user_request_id')
+  async acceptJoinFolder(
+    @TokenPayload() tokenPayload: TokenPayloadProps,
+    @Param('folder_user_request_id') folder_user_request_id: string,
+  ) {
+    await this.userService.acceptJoinFolder(
+      +folder_user_request_id,
+      tokenPayload,
+    );
+    return this.successResponse({});
+  }
+
+
+  @Post('reject-join-folder/:folder_user_request_id')
+  async rejectJoinFolder(
+    @TokenPayload() tokenPayload: TokenPayloadProps,
+    @Param('folder_user_request_id') folder_user_request_id: string,
+  ) {
+    await this.userService.rejectJoinFolder(
+      +folder_user_request_id,
+      tokenPayload,
+    );
+    return this.successResponse({});
+  }
+
+  
 }
--- src/user/users.service.ts
+++ src/user/users.service.ts
@@ -1,13 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UserEntity } from 'src/auth/entities/common/user-entity';
+import { TokenPayloadProps } from 'src/base-module/token-payload-interface';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RedisService } from 'src/redis/redis.service';
 
 @Injectable()
 export class UsersService {
   public users: any;
-    
-  constructor(private prisma: PrismaService,private redis: RedisService,) {
+
+  constructor(private prisma: PrismaService, private redis: RedisService) {
     this.users = [
       {
         userId: 1,
@@ -23,9 +29,89 @@ export class UsersService {
   }
 
   async user() {
-    this.redis.addToBlacklist('e12ee12qwedwkcabweye1x')
+    this.redis.addToBlacklist('e12ee12qwedwkcabweye1x');
     let usersFromDatabase = await this.prisma.user.findMany();
     return UserEntity.collect(usersFromDatabase);
+  }
+
+  async folderRequest({ user }: TokenPayloadProps) {
+    const foldersRequests = await this.prisma.userFolderRequest.findMany({
+      where: {
+        user_id: user.id,
+      },
+    });
+    return foldersRequests;
+  }
+
+  private async findFolderUserRequestByAuthUser(
+    folderUserRequestId: number,
+    { user }: TokenPayloadProps,
+  ) {
+    const folderUserRequest = await this.prisma.userFolderRequest.findFirst({
+      where: {
+        id: folderUserRequestId,
+      },
+      select: {
+        folder_id: true,
+        user_id: true,
+      },
+    });
+    if (!folderUserRequest) {
+      throw new NotFoundException('Request not found');
+    }
+
+    if (folderUserRequest.user_id !== user.id) {
+      throw new UnauthorizedException('You cannot perform this action');
+    }
+    return folderUserRequest;
+  }
+
+  async acceptJoinFolder(
+    folderUserRequestId: number,
+    TokenPayloadProps: TokenPayloadProps,
+  ) {
+    const folderUserRequest = await this.findFolderUserRequestByAuthUser(
+      folderUserRequestId,
+      TokenPayloadProps,
+    );
+
+    const userFolderRoleId = await this.prisma.folderRole.findFirst({
+      where: {
+        name: 'user',
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    await this.prisma.$transaction([
+      this.prisma.userFolder.create({
+        data: { ...folderUserRequest, folder_role_id: userFolderRoleId.id },
+      }),
+      this.prisma.userFolderRequest.delete({
+        where: {
+          id: folderUserRequestId,
+          user_id: TokenPayloadProps.user.id,
+        },
+      }),
+    ]);
+  }
+
+  async rejectJoinFolder(
+    folderUserRequestId: number,
+    TokenPayloadProps: TokenPayloadProps,
+  ) {
+    const folderUserRequest = await this.findFolderUserRequestByAuthUser(
+      folderUserRequestId,
+      TokenPayloadProps,
+    );
+
+    await this.prisma.userFolderRequest.delete({
+      where: {
+        id: folderUserRequestId,
+        user_id: TokenPayloadProps.user.id,
+      },
+    });
   }
 
   async findOne(username) {
