6981f6cecaa78c1e6ed91da5c21a6930e7849170
Amgad226 <amgad.wr.1@gmail.com>
2023-12-07 20:56:21 +0300

    add user folder data to folder seeder

--- prisma/seeders/folder-seeder.ts
+++ prisma/seeders/folder-seeder.ts
@@ -1,11 +1,35 @@
 import { log } from 'console';
 
 export async function folderSeeder(prisma) {
+  const amgad = await prisma.user.findFirst({
+    where: {
+      email: 'amgad@gmail.com',
+    },
+    select: {
+      id: true,
+    },
+  });
+  const folder_admin_role = await prisma.folderRole.findFirst({
+    where: {
+      name: 'admin',
+    },
+    select: {
+      id: true,
+    },
+  });
+
   await prisma.folder.create({
     data: {
+      UserFolder: {
+        create: {
+          user_id: amgad.id,
+          folder_role_id: folder_admin_role.id,
+        },
+      },
       name: 'root folder',
       logo: '',
       driveFolderID: '1T_0BsIBtv4nywGDHAB2yQocw9RRhceUw',
     },
   });
+  
 }
