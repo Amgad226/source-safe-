1fd07afae9e40528e581eedbb6d8b39cf0d7443d
Amgad226 <amgad.wr.1@gmail.com>
2023-12-09 00:00:12 +0300

    serve file in upload staticly

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { join } from 'path';
-import * as express from 'express'; // Import express module
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use('/upload', express.static(join(__dirname, '..', 'upload')));
-
+  app.use('/upload', express.static(join(__dirname, '..', '..', 'upload')));
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
 
