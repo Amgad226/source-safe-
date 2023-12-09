a15e51fc56f259d206d161bf6a009b505bc51377
Amgad226 <amgad.wr.1@gmail.com>
2023-12-10 00:20:27 +0300

    [Fix] (src/base) fix seed null to collectData function

--- src/base-module/base-entity.ts
+++ src/base-module/base-entity.ts
@@ -1,4 +1,5 @@
 export class BaseEntity {}
 export function collectDataBy(EntityClass: any, items: any[]): any[] {
+  if(items==null )return [];
   return items.map((item) => new EntityClass(item));
 }
