f48b98132ea44019042c740336d4c8fc3f2c06d0
Amgad226 <amgad.wr.1@gmail.com>
2023-12-11 23:48:17 +0300

    add created at to folder

--- src/folder/entities/base-folder.entity.ts
+++ src/folder/entities/base-folder.entity.ts
@@ -5,14 +5,16 @@ export class BaseFolderEntity extends BaseEntity {
   name: string;
   logo: string;
   driveFolderID: string;
+  created_at: Date;
 
-  constructor({ id, folder_id, name, logo, driveFolderID }) {
+  constructor({ id, folder_id, name, logo, driveFolderID,created_at }) {
     super();
     this.id = id;
     this.folder_id = folder_id;
     this.name = name;
     this.logo = logo;
     this.driveFolderID = driveFolderID;
+    this.created_at = created_at;
   }
 
 }
