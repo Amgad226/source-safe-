2d20c890ccedc1f7f4bdcbc6b623d1401222ddcc
Amgad226 <amgad.wr.1@gmail.com>
2023-12-29 00:24:59 +0300

    [Feature] (base-module) create helper to manage date

---
+++ src/base-module/helpers.ts
@@ -0,0 +1,15 @@
+export function formatTimestamp(timestamp: number): string {
+    const date = new Date(timestamp);
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      hour12: false,
+    };
+  
+    return date.toLocaleString(undefined, options);
+  }
+  
