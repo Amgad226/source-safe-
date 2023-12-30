9f889c1b40d32da339e4421be5ff66729ae7c9bf
Amgad226 <amgad.wr.1@gmail.com>
2023-12-31 02:32:51 +0300

    [Deploy] update code to deploy to render V1.0.4

--- main.ts
+++ main.ts
@@ -3,10 +3,13 @@ import { AppModule } from './src/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { join } from 'path';
 import * as express from 'express';
+import { log } from 'console';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use('/upload', express.static(join(__dirname, '..', '..', 'upload')));
+  log(12312,__dirname)
+  console.log(join(__dirname,  '..', 'upload'))
+  app.use('/upload', express.static(join(__dirname,  '..', 'upload')));
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
 
