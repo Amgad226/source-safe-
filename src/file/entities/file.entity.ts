575c499795da3617a3e365991de215a0c115521c
Amgad226 <amgad.wr.1@gmail.com>
2023-12-30 22:01:55 +0300

    [Fix] (file)  fix returning data in file entity

--- src/file/entities/file.entity.ts
+++ src/file/entities/file.entity.ts
@@ -3,13 +3,14 @@ import { FileVersionEntity } from './file-version.entity';
 import { BaseFileEntity } from './base-file.entity';
 import { FolderWithMemberEntity } from 'src/folder/entities/folder-with-members.entity';
 import { FileStatisticWithUserEntity } from './file-statistic-with-user.entity';
+import { isArray } from 'class-validator';
 
 export class FileEntity extends BaseFileEntity {
   folder?: FolderWithMemberEntity;
   file_versions: FileVersionEntity[];
-  full_size: number|string;
-  latest_size: number|string;
-  last_action_on_file:FileStatisticWithUserEntity
+  full_size: number | string;
+  latest_size: number | string;
+  last_action_on_file: FileStatisticWithUserEntity;
   constructor({
     id,
     name,
@@ -21,7 +22,7 @@ export class FileEntity extends BaseFileEntity {
     Folder = null,
     full_size = null,
     latest_size = null,
-    FileStatistic=null
+    FileStatistic = null,
   }) {
     super({
       id,
@@ -34,10 +35,21 @@ export class FileEntity extends BaseFileEntity {
     });
     this.folder = new FolderWithMemberEntity(Folder) ?? null;
     this.full_size = full_size ?? 'not_loaded_data';
-    this.latest_size = latest_size ?? 'not_loaded_data';
+    if (latest_size != null) {
+      this.latest_size = latest_size;
+    } else {
+      if (isArray(FileVersion)) {
+        this.latest_size = FileVersion[0]?.size;
+      } else {
+        this.latest_size = 'not_loaded_data';
+      }
+    }
+
     this.file_versions = collectDataBy(FileVersionEntity, FileVersion);
-    if(FileStatistic!= null){
-      this.last_action_on_file= new FileStatisticWithUserEntity(FileStatistic[0]);
+    if (FileStatistic != null) {
+      this.last_action_on_file = new FileStatisticWithUserEntity(
+        FileStatistic[0],
+      );
     }
   }
 }
